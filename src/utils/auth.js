d3788933b45618f345ab0840de3a5cf0d95bc4ec
Leonid <leonid0991@gmail.com>
2023-03-19 20:23:05 +0200

    auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,37 @@
-import { Route, Switch } from "react-router-dom";
+const BASE_URL = "https://register.nomoreparties.co";
 
-function Auth() {
-  return (
-    <Switch>
-      <Route exact path="/"></Route>
-      <Route path="/signup"></Route>
-      <Route path="/signin"></Route>
-    </Switch>
+function checkResponse(res) {
+  if (res.ok) {
+      return response.json();
+  }
+  return Promise.reject(
+      `something goes wrong: ${res.status} ${res.statusText}`
   );
 }
 
-export default Auth;
+export async function register(email, password) {
+  const res = await fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: password,
+      email: email,
+    }),
+  });
+  return checkResponse(res);
+}
+
+export async function logIn(email, password) {
+  const res = await fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      password: password,
+      email: email,
+    }),
+  });
+  return checkResponse(res);
+}
